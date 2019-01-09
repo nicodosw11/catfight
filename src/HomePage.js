922fbff327b2ece2909106333d26e7e955beec2f
Nicolas Dotto <nicolas.dotto@hotmail.com>
2019-01-09 17:14:28 +0000

    renders HomePage title

---
+++ src/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <h1>Cats vs Dogs: the fight!</h1>
+    );
+  }
+}
+
+export default HomePage;
