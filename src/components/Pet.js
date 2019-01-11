eca6092de81cd7366a86ad3cb9cf0cf8de75fc28
Nicolas Dotto <nicolas.dotto@hotmail.com>
2019-01-11 11:00:19 +0000

    pet component does not need to be a class component but just a stateless functional component returning a react element

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Pet extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div style={inlineStyle}>
-        <h3>{this.props.petName}: Likes: {this.props.likesCount}</h3>
-        <img style={{height: 400, width: 400}} src={this.props.petImageUrl} alt="" />
-        <br />
-        <button style={btnStyle} value={this.props.petName} onClick={this.props.onLikeBtnClick}>Like</button>
-        <button style={btnStyle} value={this.props.petName} onClick={this.props.onDislikeBtnClick}>Dislike</button>
-      </div>
-    );
-  }
+const Pet = props => {
+  return (
+    <div style={inlineStyle}>
+      <h3>{props.petName}: Likes: {props.likesCount}</h3>
+      <img style={{height: 400, width: 400}} src={props.petImageUrl} alt="" />
+      <br />
+      <button style={btnStyle} value={props.petName} onClick={props.onLikeBtnClick}>Like</button>
+      <button style={btnStyle} value={props.petName} onClick={props.onDislikeBtnClick}>Dislike</button>
+    </div>
+  )
 }
 
 const inlineStyle = {
